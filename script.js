2d50950e4cdd33bc62d5733717a05d853d98ba61
Denis Neves <denis.sneves@outlook.com>
2023-01-19 22:09:47 -0300

    Salvado dados na armazenamento do navegador

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: [
-    "01-01",
-    "01-02",
-    "01-03",
-    "01-04",
-    "01-05",
-    "01-06",
-    "01-07",
-    "01-08",
-    "01-09",
-    "01-10",
-  ],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("Habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("Habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
